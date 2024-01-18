a5fbc4245b367b22944821ec9efcdbc65fc9bdf0
Kumampet <kumampet@gmail.com>
2024-01-19 03:09:56 +0900

    pagesルーターでやりなおし

---
+++ my-app/src/pages/about.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Test from "../components/test";
+
+export default function About() {
+  return (
+    <React.Fragment>
+      <h1>About Page</h1>
+      <Test/>
+    </React.Fragment>
+  );
+}
